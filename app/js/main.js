9c99dd01532e25c108deb2659bd614b6039b5c91
upwlqwp <upwlqwp@bk.ru>
2022-10-28 18:25:59 +0300

    header main: section- slider, section-categories footer

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+  $(".slider__inner").slick({
+    arrows: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+  });
+});
+
+var mixer = mixitup(".categories__filtr");
