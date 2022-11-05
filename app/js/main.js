12b72e3bf8099f41ca63d7b671d21780b84b3616
upwlqwp <upwlqwp@bk.ru>
2022-11-05 11:00:24 +0300

    new edits

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,12 @@
 $(function () {
   $(".hero-slider__inner").slick({
     infinite: false,
-    prevArrow: '<button type="button" class="arrows arrow--left"><svg class="arrow"><use xlink:href="images/sprite.svg#arrow"></use></svg></button>',
-    nextArrow: '<button type="button" class="arrows arrow--right"><svg class="arrow"><use xlink:href="images/sprite.svg#arrow"></use></svg></button>',
+    prevArrow: '<button type="button" class="hero-slider__arrow hero-slider__arrow--left"><svg class="arrow"><use xlink:href="images/sprite.svg#arrow"></use></svg></button>',
+    nextArrow: '<button type="button" class="hero-slider__arrow hero-slider__arrow--right"><svg class="arrow"><use xlink:href="images/sprite.svg#arrow"></use></svg></button>',
     speed: 300,
     slidesToShow: 1,
     adaptiveHeight: true,
   });
 });
 
-var mixer = mixitup(".products");
+var mixer = mixitup(".top-products__list");
